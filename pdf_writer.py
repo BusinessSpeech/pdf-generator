ed9cce5797c2dafe9d128244e5ddb79dec0965d4
Stepan Tarasevich <stepan.tarasevich@jetbrains.com>
2020-01-11 02:11:25 +0300

    Add images to generated pdfs

--- pdf_writer.py
+++ pdf_writer.py
@@ -7,6 +7,9 @@ from config import config
 
 reportlab.rl_config.warnOnMissingFontGlyphs = 0
 
+HEIGHT = A4[1]
+print(A4)
+
 FONT_NAME = 'OsnovaPro'
 FONT_NAME_LIGHT = 'OsnovaProLight'
 
@@ -17,7 +20,7 @@ pdfmetrics.registerFont(TTFont(FONT_NAME_LIGHT, 'OsnovaProLight.ttf'))
 def create_multipage_pdf(filename, participants_list, trainer_name, training_name, date, place):
     pdf_config = config['pdf'] if 'pdf' in config else dict()
     mid_width = A4[0] / 2
-    c = Canvas(filename, pagesize=A4, bottomup=0)
+    c = Canvas(filename, pagesize=A4, bottomup=1)
     for participant in participants_list:
         create_page(c, pdf_config, mid_width, participant, training_name, trainer_name, date, place)
         c.showPage()
@@ -34,12 +37,32 @@ def create_pdf(filename, trainee_name, trainer_name, training_name, date, place)
     c.save()
 
 
+def draw_quotes(c, pdf_config):
+    quote_y = int(pdf_config['quote_y'])
+    right_quote_x = int(pdf_config['right_quote_x'])
+    left_quote_x = int(pdf_config['left_quote_x'])
+    quote_width = int(pdf_config['quote_width'])
+    quote_height = int(pdf_config['quote_height'])
+    c.drawImage("./quote-right.png", right_quote_x, quote_y, quote_width, quote_height, mask='auto')
+    c.drawImage("./quote-left.png", left_quote_x, quote_y, quote_width, quote_height, mask='auto')
+
+
+def draw_logo(c, pdf_config, mid_width):
+    logo_height = int(pdf_config['logo_height'])
+    logo_width = int(pdf_config['logo_width'])
+    logo_y = int(pdf_config['logo_y'])
+    logo_x = int(mid_width - logo_width / 2)
+    c.drawImage("./logo.png", logo_x, logo_y, logo_width, logo_height, mask='auto')
+
+
 def create_page(c, pdf_config, mid_width, trainee_name, training_name, trainer_name, date, place):
     print_supplementary_text(c, pdf_config, mid_width)
     print_trainee_name(c, pdf_config, mid_width, trainee_name)
     print_training_title(c, pdf_config, mid_width, training_name)
     print_date_and_place(c, pdf_config, mid_width, date, place)
     print_trainer_name(c, pdf_config, trainer_name)
+    draw_logo(c, pdf_config, mid_width)
+    draw_quotes(c, pdf_config)
 
 
 def print_supplementary_text(c, pdf_config, mid_width):
@@ -73,11 +96,11 @@ def print_training_title(c, pdf_config, mid_width, training_title):
     c.setFont(FONT_NAME, font_size)
     c.setFillColorRGB(0.0, 0.0, 0.0)
     training_name_lines = [l.strip() for l in training_title.split('\n') if l.strip() != '']
-    start_top = text_mid_y - (len(training_name_lines) - 1) / 2 * line_height
+    start_top = text_mid_y + (len(training_name_lines) - 1) / 2 * line_height  # TODO: +
     line_top = start_top
     for line in training_name_lines:
         c.drawCentredString(mid_width, line_top, line)
-        line_top += line_height
+        line_top -= line_height
 
 
 def print_date_and_place(c, pdf_config, mid_width, date, place):
@@ -86,7 +109,7 @@ def print_date_and_place(c, pdf_config, mid_width, date, place):
     line_height = int(pdf_config['training_place_date_line_height'])
     c.setFont(FONT_NAME, font_size)
     c.drawCentredString(mid_width, text_y, date)
-    c.drawCentredString(mid_width, text_y + line_height, place)
+    c.drawCentredString(mid_width, text_y - line_height, place)
 
 
 def print_trainer_name(c, pdf_config, trainer_name):
@@ -99,7 +122,7 @@ def print_trainer_name(c, pdf_config, trainer_name):
 
     c.setFont(FONT_NAME_LIGHT, font_size)
     c.drawString(left, top, 'Бизнес-тренер:')
-    second_line_y = top + line_height
+    second_line_y = top - line_height
     t = c.beginText(left, second_line_y)
     t.textOut(trainer_name)
     new_x, _ = t.getCursor()
@@ -107,7 +130,7 @@ def print_trainer_name(c, pdf_config, trainer_name):
 
     signature_line_x = new_x + signature_line_gap
     p = c.beginPath()
-    p.moveTo(signature_line_x, second_line_y + 0.2)
+    p.moveTo(signature_line_x, second_line_y - 0.2)
     p.lineTo(signature_line_x + signature_line_length, second_line_y + 0.2)
     c.setLineWidth(0.5)
     c.drawPath(p)
